f9ba2a703283ef5306c22dd9f8266eef6f351977
Michelle Johnson <bob1234girl@gmail.com>
2019-10-25 15:27:54 -0500

    got list of parks and their states to show on the Dom

--- src/scripts/addParksToDom.js
+++ src/scripts/addParksToDom.js
@@ -1,22 +1,16 @@
 // string that enters search form into HTML
 const buildAndAppendPark = (parksObject) => {
-    const parkForm = `
+    return `
         <article>
             <h3>${parksObject.name}</h3>
             <p>${parksObject.state}</p>
         </article>
     `
-    console.log(parkForm)
-
-    // inserts string into div
-    const parkContainerDiv = document.querySelector(".national-parks-container")
-    parkContainerDiv.innerHTML = parkForm
-    
   }
 
-  buildAndAppendPark()
+  // buildAndAppendPark()
 
-  const addParksToDOM = (html) => {
+  const addParksToDom = (html) => {
     const parksContainer = document.querySelector(".national-parks-container");
     parksContainer.innerHTML += html;
   }
--- src/scripts/parkApi.js
+++ src/scripts/parkApi.js
@@ -9,6 +9,7 @@ fetch("http://localhost:8088/parks")
     fetch("http://localhost:8088/parks")
     .then(parks => parks.json())
     .then(parsedParks => {
+        console.log(parsedParks)
         parsedParks.forEach(parks => {
             const ParksAsHTML = buildAndAppendPark(parks)
             addParksToDom(ParksAsHTML)
